5542df7e78b99bf0964dac3a0d10c73e16c146bb
snekw <dev.snekw@gmail.com>
2021-07-05 15:23:07 +0300

    Add new example

--- README.md
+++ README.md
@@ -1,2 +1,120 @@
-# lcjs-example-0704-customCursorStackedY
+# JavaScript Custom Cursor with Stacked Y Dashboard
+
+![JavaScript Custom Cursor with Stacked Y Dashboard](customCursorStackedY.png)
+
+This demo application belongs to the set of examples for LightningChart JS, data visualization library for JavaScript.
+
+LightningChart JS is entirely GPU accelerated and performance optimized charting library for presenting massive amounts of data. It offers an easy way of creating sophisticated and interactive charts and adding them to your website or web application.
+
+The demo can be used as an example or a seed project. Local execution requires the following steps:
+
+- Make sure that relevant version of [Node.js](https://nodejs.org/en/download/) is installed
+- Open the project folder in a terminal:
+
+        npm install              # fetches dependencies
+        npm start                # builds an application and starts the development server
+
+- The application is available at *http://localhost:8080* in your browser, webpack-dev-server provides hot reload functionality.
+
+
+## Description
+
+This example serves as an example for custom data cursor implementation in _stacked Y charts dashboard_, where there are several XY charts stacked vertically on each other.
+
+In this case, the X Axis of each chart are _synchronized_, and when user mouse is above the chart the data of all series `Y` coordinates is shown at the respective `X` coordinate.
+
+Each `LineSeries` contains 100 000 unique data points, coming to a total of **300 000** data points. Regardless, the interaction with cursor is instantaneous.
+
+Custom cursors are most importantly based on `LCJS` methods that allow solving nearest data points in series from any supplied location.
+Custom user interactions and data point solving require solid understanding of different coordinate systems in web and `LCJS`, which is the primary reason this example exists;
+
+```javascript
+// Add custom action when user moves mouse over series area.
+chart.onSeriesBackgroundMouseMove((_, event) => {
+  // `event` is a native JavaScript event, which packs the active mouse location in `clientX` and `clientY` properties.
+  const mouseLocationClient = { x: event.clientX, y: event.clientY };
+
+  // Before using client coordinates with LCJS, the coordinates have to be translated relative to the LCJS engine.
+  const mouseLocationEngine = chart.engine.clientLocation2Engine(
+    mouseLocationClient.x,
+    mouseLocationClient.y
+  );
+
+  // Now that the coordinates are in the correct coordinate system, they can be used
+  // to solve data points, or further translated to any Axis.
+
+  // (1) Translate mouse location an Axis.
+  const mouseLocationAxis = translatePoint(
+    mouseLocationEngine,
+    // Source coordinate system.
+    chart.engine.scale,
+    // Target coordinate system.
+    series[0].scale
+  );
+
+  // (2) Solve nearest data point from a series to the mouse.
+  const nearestDataPoint = series.solveNearestFromScreen(mouseLocationEngine);
+  // `nearestDataPoint` is either `undefined`, or an object 
+  // which contains a collection of information about the solved data point.
+});
+```
+
+The default cursor can only display data from one chart at a time, but using the `LCJS` methods, the cursor can be configured to display data from all independent charts at the same time.
+
+More custom cursor examples can be found by looking for "cursor" tag in the _Interactive Examples_ gallery.
+
+
+## API Links
+
+* [Lightning Chart top reference]
+* [Dashboard]
+* [Coordinate system translation method]
+* [Auto cursor modes]
+* [UI element builders]
+* [UI layout builders]
+* [UI backgrounds]
+* [UI position origin]
+* [Color factory css]
+* [Solid fill style]
+* [Solid line style]
+* [Chart XY]
+* [Axis XY]
+* [Custom tick]
+
+
+## Support
+
+If you notice an error in the example code, please open an issue on [GitHub][0] repository of the entire example.
+
+Official [API documentation][1] can be found on [Arction][2] website.
+
+If the docs and other materials do not solve your problem as well as implementation help is needed, ask on [StackOverflow][3] (tagged lightningchart).
+
+If you think you found a bug in the LightningChart JavaScript library, please contact support@arction.com.
+
+Direct developer email support can be purchased through a [Support Plan][4] or by contacting sales@arction.com.
+
+[0]: https://github.com/Arction/
+[1]: https://www.arction.com/lightningchart-js-api-documentation/
+[2]: https://www.arction.com
+[3]: https://stackoverflow.com/questions/tagged/lightningchart
+[4]: https://www.arction.com/support-services/
+
+© Arction Ltd 2009-2020. All rights reserved.
+
+
+[Lightning Chart top reference]: https://www.arction.com/lightningchart-js-api-documentation/v3.0.1/interfaces/lightningchart.html
+[Dashboard]: https://www.arction.com/lightningchart-js-api-documentation/v3.0.1/classes/dashboard.html
+[Coordinate system translation method]: https://www.arction.com/lightningchart-js-api-documentation/v3.0.1/globals.html#translatepoint
+[Auto cursor modes]: https://www.arction.com/lightningchart-js-api-documentation/v3.0.1/enums/autocursormodes.html
+[UI element builders]: https://www.arction.com/lightningchart-js-api-documentation/v3.0.1/globals.html#uielementbuilders
+[UI layout builders]: https://www.arction.com/lightningchart-js-api-documentation/v3.0.1/globals.html#uilayoutbuilders
+[UI backgrounds]: https://www.arction.com/lightningchart-js-api-documentation/v3.0.1/globals.html#uibackgrounds
+[UI position origin]: https://www.arction.com/lightningchart-js-api-documentation/v3.0.1/globals.html#uiorigins
+[Color factory css]: https://www.arction.com/lightningchart-js-api-documentation/v3.0.1/globals.html#colorcss
+[Solid fill style]: https://www.arction.com/lightningchart-js-api-documentation/v3.0.1/classes/solidfill.html
+[Solid line style]: https://www.arction.com/lightningchart-js-api-documentation/v3.0.1/classes/solidline.html
+[Chart XY]: https://www.arction.com/lightningchart-js-api-documentation/v3.0.1/classes/chartxy.html
+[Axis XY]: https://www.arction.com/lightningchart-js-api-documentation/v3.0.1/classes/axis.html
+[Custom tick]: https://www.arction.com/lightningchart-js-api-documentation/v3.0.1/classes/customtick.html
 
---
+++ src/index.js
@@ -0,0 +1,314 @@
+/*
+ * LightningChartJS example that showcases custom cursor implementation with stacked Y charts.
+ */
+// Import LightningChartJS
+const lcjs = require("@arction/lcjs");
+
+// Extract required parts from LightningChartJS.
+const {
+   lightningChart,
+   emptyFill,
+   emptyLine,
+   AxisTickStrategies,
+   AutoCursorModes,
+   SolidFill,
+   SolidLine,
+   ColorCSS,
+   ColorRGBA,
+   translatePoint,
+   UILayoutBuilders,
+   UIElementBuilders,
+   UIBackgrounds,
+   UIOrigins,
+   Themes,
+} = lcjs;
+
+// Import data-generator from 'xydata'-library.
+const {
+   createProgressiveTraceGenerator
+} = require("@arction/xydata");
+
+const channels = ["Ch 1", "Ch 2", "Ch 3"];
+const channelCount = channels.length;
+
+// Create Dashboard.
+const dashboard = lightningChart().Dashboard({
+   // theme: Themes.dark
+   numberOfRows: channelCount,
+   numberOfColumns: 1,
+});
+
+// Map XY-charts to Dashboard for each channel.
+const charts = channels.map((channelName, i) => {
+   const chart = dashboard.createChartXY({
+      columnIndex: 0,
+      rowIndex: i,
+      columnSpan: 1,
+      rowSpan: 1,
+   });
+
+   // Disable default auto cursor.
+   chart.setAutoCursorMode(AutoCursorModes.disabled);
+
+   if (i === 0) {
+      chart.setTitle("Stacked Y Dashboard with Custom Cursor");
+   } else {
+      chart.setTitleFillStyle(emptyFill);
+   }
+
+   // Only display X ticks for bottom chart.
+   if (i !== channelCount - 1) {
+      chart.getDefaultAxisX().setTickStrategy(AxisTickStrategies.Empty);
+   }
+
+   // Sync X axes of stacked charts by adding an invisible tick to each Y axis with preset length.
+   chart
+      .getDefaultAxisY()
+      .addCustomTick(UIElementBuilders.AxisTick)
+      // Preset length is configured with tick length property.
+      .setTickLength(50)
+      // Following code makes the tick invisible.
+      .setTextFormatter(() => "")
+      .setGridStrokeStyle(emptyLine)
+      .setMarker((marker) =>
+         marker.setTickStyle(new SolidFill({
+            color: ColorRGBA(0, 0, 0, 0)
+         }))
+      );
+
+   return chart;
+});
+
+// The bottom X Axis, which shows ticks for all stacked charts.
+const axisX = charts[charts.length - 1].getDefaultAxisX();
+
+// Add progressive line series to each chart.
+const seriesList = charts.map((chart, i) =>
+   chart.addLineSeries({
+      dataPattern: {
+         // 'ProgressiveX' = every data points X value is higher than the previous ones X value.
+         pattern: "ProgressiveX",
+         // 'regularProgressiveStep: true' = the step between two consecutive data points is always same.
+         regularProgressiveStep: true,
+      },
+   })
+);
+
+// Generate and push data to each line series.
+seriesList.forEach((series, i) =>
+   createProgressiveTraceGenerator()
+   .setNumberOfPoints(100 * 1000)
+   .generate()
+   .toPromise()
+   .then((data) => {
+      series.add(data);
+   })
+);
+
+
+// Code for synchronizing all X Axis intervals in stacked XY charts.
+let isAxisXScaleChangeActive = false
+const syncAxisXEventHandler = (axis, start, end) => {
+   if (isAxisXScaleChangeActive) return
+   isAxisXScaleChangeActive = true
+
+   // Find all other X Axes.
+   const otherAxes = charts
+      .map(chart => chart.getDefaultAxisX())
+      .filter(axis2 => axis2 !== axis)
+
+   // Sync other X Axis intervals.  
+   otherAxes.forEach((axis) => axis
+      .setInterval(start, end, false, true)
+   )
+
+   isAxisXScaleChangeActive = false
+}
+
+
+// Create UI elements for custom cursor.
+const resultTable = dashboard
+   .addUIElement(
+      UILayoutBuilders.Column.setBackground(UIBackgrounds.Rectangle),
+      dashboard.engine.scale
+   )
+   .setMouseInteractions(false)
+   .setOrigin(UIOrigins.LeftBottom)
+   .setMargin(5)
+   .setBackground((background) =>
+      background.setStrokeStyle(
+         new SolidLine({
+            thickness: 1,
+         })
+      )
+   );
+
+const rowX = resultTable
+   .addElement(UILayoutBuilders.Row)
+   .addElement(UIElementBuilders.TextBox)
+   .setTextFont((font) => font.setSize(14));
+
+const rowsY = seriesList.map((el, i) => {
+   return resultTable
+      .addElement(UILayoutBuilders.Row)
+      .addElement(UIElementBuilders.TextBox)
+      .setTextFont((font) => font.setSize(14));
+});
+
+const tickX = charts[channelCount - 1]
+   .getDefaultAxisX()
+   .addCustomTick()
+   .setGridStrokeStyle(
+      new SolidLine({
+         thickness: 1,
+         fillStyle: new SolidFill({
+            color: ColorCSS("gray"),
+         }),
+      })
+   );
+
+const ticksX = [];
+charts.forEach((chart, i) => {
+   if (i !== channelCount - 1) {
+      ticksX.push(
+         chart
+         .getDefaultAxisX()
+         .addConstantLine()
+         .setValue(0)
+         .setMouseInteractions(false)
+         .setStrokeStyle(
+            new SolidLine({
+               thickness: 1,
+               fillStyle: new SolidFill({
+                  color: ColorCSS("gray"),
+               }),
+            })
+         )
+      );
+   }
+});
+
+const ticksY = seriesList.map((el, i) => {
+   return charts[i]
+      .getDefaultAxisY()
+      .addCustomTick()
+      .setGridStrokeStyle(
+         new SolidLine({
+            thickness: 1,
+            fillStyle: new SolidFill({
+               color: ColorCSS("gray"),
+            }),
+         })
+      );
+});
+
+const setCustomCursorVisible = (visible) => {
+   if (!visible) {
+      resultTable.dispose();
+      tickX.dispose();
+      ticksY.forEach((el) => el.dispose());
+      ticksX.forEach((el) => el.dispose());
+   } else {
+      resultTable.restore();
+      tickX.restore();
+      ticksY.forEach((el) => el.restore());
+      ticksX.forEach((el) => el.restore());
+   }
+}
+// Hide custom cursor components initially.
+setCustomCursorVisible(false)
+
+
+// Implement custom cursor logic with events.
+charts.forEach((chart, i) => {
+   
+   const AxisX =  chart.getDefaultAxisX()
+
+   chart.onSeriesBackgroundMouseMove((_, event) => {
+      // mouse location in web page
+      const mouseLocationClient = {
+         x: event.clientX,
+         y: event.clientY,
+      };
+      // Translate mouse location to LCJS coordinate system for solving data points from series, and translating to Axes.
+      const mouseLocationEngine = chart.engine.clientLocation2Engine(
+         mouseLocationClient.x,
+         mouseLocationClient.y
+      );
+
+      // Find the nearest data point to the mouse.
+      const nearestDataPoints = seriesList.map((el) =>
+         el.solveNearestFromScreen(mouseLocationEngine)
+      );
+
+      // Abort operation if any of solved data points is `undefined`.
+      if (nearestDataPoints.includes(undefined)) {
+         setCustomCursorVisible(false)
+         return
+      }
+
+      // location of nearest point of current chart
+      const nearestPointLocation =  nearestDataPoints[i].location
+
+      // Translate mouse location to dashboard scale.
+      const mouseLocationAxis = translatePoint(
+         nearestPointLocation,
+         // Source coordinate system.
+         seriesList[i].scale,
+         // Target coordinate system.
+         dashboard.engine.scale
+      );
+
+      // Set custom cursor location. 
+      resultTable.setPosition({
+         x: mouseLocationAxis.x,
+         y: mouseLocationEngine.y,
+       });
+
+
+      // Change origin of result table based on cursor location.
+      if ( nearestPointLocation.x > AxisX.getInterval().end / 1.5 ) {
+            resultTable.setOrigin(UIOrigins.RightBottom);
+      } else {
+         resultTable.setOrigin(UIOrigins.LeftBottom);
+      }
+
+      // Format result table text.
+      rowX.setText(`X: ${axisX.formatValue(nearestDataPoints[i].location.x)}`);
+      rowsY.forEach((rowY, i) => {
+         rowY.setText(
+            `Y${i}: ${charts[i]
+            .getDefaultAxisY()
+            .formatValue(nearestDataPoints[i].location.y)}`
+         );
+      });
+
+      // Position custom ticks.
+      tickX.setValue(nearestDataPoints[i].location.x);
+      ticksX.forEach((tick, i) => {
+         tick.setValue(tickX.getValue());
+      });
+      ticksY.forEach((tick, i) => {
+         tick.setValue(nearestDataPoints[i].location.y);
+      });
+
+      // Display cursor.
+      setCustomCursorVisible(true)
+   });
+
+   // sync all the axes
+   const axis = chart.getDefaultAxisX()
+   axis.onScaleChange((start, end) =>
+      syncAxisXEventHandler(axis, start, end)
+   )
+
+   // hide custom cursor and ticks if mouse leaves chart area
+   chart.onSeriesBackgroundMouseLeave((_, e) => {
+      setCustomCursorVisible(false)
+   });
+
+   // hide custom cursor and ticks on Drag
+   chart.onSeriesBackgroundMouseDragStart((_, e) => {
+      setCustomCursorVisible(false)
+   });
+});
